21db40c8bf4c2a09ac027ad66da8ae48efde31e2
TailonDaSilva <tailontegner@live.com>
2025-02-05 19:58:24 -0300

    Alterado produto para maça

--- js/main.js
+++ js/main.js
@@ -4,20 +4,20 @@ const containerPai = document.getElementById('cards');
 
 const cardMaca = new ClassCard(
   containerPai,
-  'Fone de Ouvido',                               //TITULO DO CARD
+  'Maçã Importada',                               //TITULO DO CARD
   'Cod.: 54785-66',                               //SUB-TITULO DO CARD
-  '124,99',                                         //PREÇO DO PRODUTO
+  '7,99',                                         //PREÇO DO PRODUTO
   'Comprar Este Produto',                         // TEXTO DO BOTÃO
-  'blue',                                          //COR DO FUNDO DO CARD COM IMG PRINCIPAL
-  'blue',                                         //COR DO BOTÃO
+  'red',                                          //COR DO FUNDO DO CARD COM IMG PRINCIPAL
+  'red',                                         //COR DO BOTÃO
   'white',                                       //COR DO CONTAINER PRINCIPAL DO CARD
   'white',                                        //COR DO TEXTO DO BOTÃO
   'white',                                        //COR DO CONTAINER DAS MINIATURAS
-  './assets/img/fone/img_principal.png',          //URL DA IMG PRINCIPAL
-  './assets/img/fone/img_mini_1.png',             //URL DA IMG MINIATURA 1 - ESQUEDA PARA DIREITA
-  './assets/img/fone/img_mini_2.png',             //URL DA IMG MINIATURA 2 - ESQUEDA PARA DIREITA
-  './assets/img/fone/img_mini_3.png',             //URL DA IMG MINIATURA 3 - ESQUEDA PARA DIREITA
-  './assets/img/fone/img_mini_4.png',             //URL DA IMG MINIATURA 4 - ESQUEDA PARA DIREITA
+  './assets/img/maça/img_principal.png',          //URL DA IMG PRINCIPAL
+  './assets/img/maça/img_mini_1.png',             //URL DA IMG MINIATURA 1 - ESQUEDA PARA DIREITA
+  './assets/img/maça/img_mini_2.png',             //URL DA IMG MINIATURA 2 - ESQUEDA PARA DIREITA
+  './assets/img/maça/img_mini_3.png',             //URL DA IMG MINIATURA 3 - ESQUEDA PARA DIREITA
+  './assets/img/maça/img_mini_4.png',             //URL DA IMG MINIATURA 4 - ESQUEDA PARA DIREITA
   'Cor: Vermelho ou Verde',                       //TEXTO DO ITEM 1 - INFORMAÇÕES DO PRODUTO
   'Preparo: Em Natura',                           //TEXTO DO ITEM 2 - INFORMAÇÕES DO PRODUTO
   'Origem: Santa Catarina',                       //TEXTO DO ITEM 3 - INFORMAÇÕES DO PRODUTO
